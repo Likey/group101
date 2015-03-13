5d1937d3e77f89b0dadf912f4472a047f50ceca3
Hsiehld <hsiehld@gmail.com>
2015-03-13 06:50:44 +0000

    generate group

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  resource :groups
+  
+  root 'groups#index'   # 把 localhost:3000/groups 設定為首頁
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
