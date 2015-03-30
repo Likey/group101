ee64db2d407f5684f87019ce6da975b49cb20126
Hsiehld <hsiehld@gmail.com>
2015-03-30 07:09:02 +0000

    user can join, or quit group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,6 +23,7 @@ class GroupsController < ApplicationController
     @group = current_user.groups.new(group_params)
     
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path, :notice => '新增討論板成功'
     else
       render :new
@@ -45,11 +46,38 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, :alert => '討論板已刪除'
   end
+  
+  def join
+    @group = Group.find(params[:id])
+    
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本討論版成功！"
+    else
+      flash[:warning] = "您已經是本討論版成員了！"
+    end
+    
+    redirect_to group_path(@group)
+  end
+  
+  def quit
+    @group = Group.find(params[:id])
+    
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "已退出本討論版！"
+    else
+      flash[:warning] = "你不是本討論版成員，怎麼退出 XD"
+    end
+    
+    redirect_to group_path(@group)
+  end
+  
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
 
 
-private
-
-def group_params
-  params.require(:group).permit(:title, :description)
-end
